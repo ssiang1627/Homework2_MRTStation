fd80907fd0462182a1de216b8a914853a4eee71b
林聖翔 <encorej44641@gmail.com>
2016-05-18 22:34:47 +0800

    first commit

---
+++ MRTStation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MRTStation
+//
+//  Created by mac on 2016/5/5.
+//  Copyright © 2016年 ssiang1627. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
